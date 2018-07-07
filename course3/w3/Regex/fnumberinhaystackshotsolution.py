2cce2596d90d3d5d617110128112492ce8c5d962
Rahul Kumar <rahulmanh@gmail.com>
2018-07-07 12:16:51 +0400

    removing test codes

--- course3/w3/Regex/fnumberinhaystackshotsolution.py
+++
@@ -1,8 +0,0 @@
-import re
-
-fileName = input("Enter name of file")
-if len(fileName) < 1:
-    fileName = "sample.txt"
-handle = open(fileName)
-data = handle.read()
-print(sum([int(num) for num in re.findall("[0-9]+",data)]))
